62cc07166680ce89766fcf3a8ba6b8d2566226cf
Joe Ho <jkho@basistech.com>
2012-11-29 13:24:27 -0500

    merged with main and fixed runtime error.

--- SummaryReportModule.cpp
+++ SummaryReportModule.cpp
@@ -1,108 +1,40 @@
 /*
-* The Sleuth Kit
-*
-* Contact: Brian Carrier [carrier <at> sleuthkit [dot] org]
-* Copyright (c) 2011-2012 Basis Technology Corporation. All Rights
-* reserved.
-*
-* This software is distributed under the Common Public License 1.0
-*/
+ * The Sleuth Kit
+ *
+ * Contact: Brian Carrier [carrier <at> sleuthkit [dot] org]
+ * Copyright (c) 2011-2012 Basis Technology Corporation. All Rights
+ * reserved.
+ *
+ * This software is distributed under the Common Public License 1.0
+ */
 
-/** \file SummaryReportModule.cpp 
-* C++ Framework module that creates a summary report with tables for all Blackboard
-* artifacts found on the Blackboard.
-*/
+/** 
+ * \file SummaryReportModule.cpp 
+ * Contains the implementation of a post-processing module that creates a blackboard artifacts report.
+ */
 
-// System includes
-#ifdef TSK_WIN32
-    #include <windows.h>
-#endif
+// Module includes
+#include "SummaryReport.h"
 
-#include <sstream>
-
-// Framework includes
+// TSK Framework includes
 #include "TskModuleDev.h"
-#include "Utilities/TskUtilities.h"
 #include "Services/TskSystemProperties.h"
 
 // Poco includes
-
-#include "Poco/FileStream.h"
+#include "Poco/Path.h"
 #include "Poco/File.h"
 #include "Poco/Exception.h"
 
+// C/C++ standard library includes 
+#include <string>
+#include <sstream>
+#include <vector>
+
 namespace
 {
-    const char *MODULE_NAME = "SummaryReport";
-    const char *MODULE_DESCRIPTION = "Creates an HTML report on data posted to the blackboard";
-    const char *MODULE_VERSION = "1.0.0";
-}
-
-// Convert reserved HTML characters to HTML entities
-static string HTMLEncode(const string& str)
-{
-    string convertedStr;
-    for (size_t i = 0; i < str.size(); i++) {
-        if (str[i] == '<')
-            convertedStr.append("&lt;");
-        else if (str[i] == '>')
-            convertedStr.append("&gt;");
-        else if (str[i] == '&')
-            convertedStr.append("&amp;");
-        else if (str[i] == '"')
-            convertedStr.append("&quot;");
-        else if (str[i] == '\'')
-            convertedStr.append("&apos;");
-        else
-            convertedStr += str[i];
-    }
-    return convertedStr;
-}
-
-static void addStyle(Poco::FileOutputStream & out){
-    out << "<style type=\"text/css\">" << std::endl <<  
-        "table.gridtable {" << std::endl <<
-        "font-family: verdana,arial,sans-serif;" << std::endl <<
-        "font-size:11px;" << std::endl <<
-        "color:#333333;" << std::endl <<
-        "border-width: 1px;" << std::endl <<
-        "border-color: #666666;" << std::endl <<
-        "border-collapse: collapse;" << std::endl <<
-        "}" << std::endl <<
-        "table.gridtable th {" << std::endl <<
-        "border-width: 1px;" << std::endl <<
-        "padding: 8px;" << std::endl <<
-        "border-style: solid;" << std::endl <<
-        "border-color: #666666;" << std::endl <<
-        "background-color: #dedede;" << std::endl <<
-        "}" << std::endl <<
-        "table.gridtable td {" << std::endl <<
-        "border-width: 1px;" << std::endl <<
-        "padding: 8px;" << std::endl <<
-        "border-style: solid;" << std::endl <<
-        "border-color: #666666;" << std::endl <<
-        "background-color: #ffffff;" << std::endl <<
-        "}" << std::endl <<
-        "h1 {" << std::endl <<
-        "font-size: 1.5em;" << std::endl <<
-        "color: #000000;" << std::endl <<
-        "font-family: Arial, sans-serif;" << std::endl <<
-        "}" << std::endl <<
-
-        "h2 {" << std::endl <<
-        "font-size: 1.2em;" << std::endl <<
-        "color: #000000;" << std::endl <<
-        "font-family: Arial, sans-serif;" << std::endl <<
-        "}" << std::endl <<
-
-        "h3 {" << std::endl <<
-        "margin-left: 0;" << std::endl <<
-        "margin-bottom: 0;" << std::endl <<
-        "font-size: 1.0em;" << std::endl <<
-        "color: #000000;" << std::endl <<
-        "font-family: Arial, sans-serif;" << std::endl <<
-        "}" << std::endl <<
-        "</style>" << std::endl;
+    const char * MODULE_NAME = "SummaryReport";
+    const char * MODULE_DESCRIPTION = "Creates an HTML report on data posted to the blackboard";
+    const char * MODULE_VERSION = "1.0.0";        
 }
 
 extern "C" 
@@ -137,241 +69,121 @@ extern "C"
         return MODULE_VERSION;
     }
 
-   /**
-    * Module initialization function. Takes a string as input that allows
-    * arguments to be passed into the module.
-    * @param arguments None yet. In the future these may be used to specify
-    * artifacts to use for the report.
-    */
+    /**
+     * Module initialization function. Receives a string of initialization arguments, 
+     * typically read by the caller from a pipeline configuration file. 
+     * Returns TskModule::OK or TskModule::FAIL. Returning TskModule::FAIL indicates 
+     * the module is not in an operational state.  
+     *
+     * @param args a string of initialization arguments.
+     * @return TskModule::OK if initialization succeeded, otherwise TskModule::FAIL.
+     */
     TskModule::Status TSK_MODULE_EXPORT initialize(const char* arguments)
-    {
+    {    
         return TskModule::OK;
     }
 
     /**
-    * Hash the file and post the results to the database. 
-    * @param pFile A pointer to a file to be processed.
-    * @returns TskModule::OK on success and TskModule::FAIL on error.
-    */
-    TskModule::Status TSK_MODULE_EXPORT report() {
-
-        try{
-            std::string outputPath;
-
-            outputPath = GetSystemProperty(TskSystemProperties::MODULE_OUT_DIR);
-            if (outputPath == "") 
-            {
-                LOGERROR(L"ReportModule Module: OutputDir System property is empty.");
-                return TskModule::FAIL;
-            }
-            outputPath.append("/Reports/");
-            Poco::File reportsFolder(outputPath);
-            if (reportsFolder.exists() == false) {
-                reportsFolder.createDirectories();
-            }
-
-            outputPath.append("SummaryReport.htm");
-
-            std::stringstream msg;
-            msg << "Logging report to " << outputPath.c_str();
-            LOGINFO(msg.str());
-            
-            Poco::FileOutputStream out(outputPath);
-
-            out << "<html>" << std::endl;
-            out << "<head>" << std::endl;
-            out << "<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />" << std::endl;
-            addStyle(out);
-
-            out << "<title>Report</title>" << std::endl;
-            out << "</head>" << std::endl;
-            out << "<body>" << std::endl;
-
-            TskBlackboard & blackboard = TskServices::Instance().getBlackboard();
-            TskImgDB & imgdb = TskServices::Instance().getImgDB();
-
-            std::stringstream condition;
-
-            out << "<h1>Sleuth Kit Framework Summary Report</h1>" << std::endl;
-
-            std::vector<std::string> names = imgdb.getImageNames();
-            out << "<h2>Image Path: " << names.front() << "</h2>" << std::endl;
-
-            out << "<h2>Image Layout</h2>" << std::endl;
-            std::list<TskVolumeInfoRecord> volumeInfoList;
-            imgdb.getVolumeInfo(volumeInfoList);
-
-            std::list<TskFsInfoRecord> fsInfoList;
-            imgdb.getFsInfo(fsInfoList);
-            TskFsInfoRecord fsInfo;
-
-            if (fsInfoList.size() == 0)
-                out << "<em>NO FILE SYSTEMS FOUND IN THE DISK IMAGE.</em>" << std::endl;
-
-            out << "<table class=\"gridtable\">" << std::endl;
-            out << "<thead>" << std::endl;
-            out << "<tr>" << std::endl;
-            out << "<th>Start Sector</th>" << std::endl;
-            out << "<th>End Sector</th>" << std::endl;
-            out << "<th>Partition Type</th>" << std::endl;
-            out << "<th>Detected FS</th>" << std::endl;
-            out << "</tr>" << std::endl;
-            out << "</thead>" << std::endl;
-
-            for (list<TskVolumeInfoRecord>::const_iterator iter = volumeInfoList.begin(); iter != volumeInfoList.end(); iter++) {
-                const TskVolumeInfoRecord & vol_info = *iter;
-                out << "<tr>" << std::endl;
-                out << "<td>" << vol_info.sect_start << "</td>" << std::endl;
-                out << "<td>" << (vol_info.sect_start + vol_info.sect_len) - 1 << "</td>" << std::endl;
-                out << "<td>" << vol_info.description << "</td>" << std::endl;
-
-                for(list<TskFsInfoRecord>::const_iterator iter2 = fsInfoList.begin(); iter2 != fsInfoList.end(); iter2++){
-                    fsInfo = (*iter2); 
-                    if(fsInfo.vol_id == vol_info.vol_id)
-                        out << "<td>" << tsk_fs_type_toname((TSK_FS_TYPE_ENUM)fsInfo.fs_type) << "</td>" << std::endl;
-                }
-
-                out << "</tr>" << std::endl;
-            }
-            out << "</table>" << std::endl;
-
-            out << "<h2>File Categories</h2>" << std::endl;
-            out << "<table class=\"gridtable\">" << std::endl;
-
-            out << "<tr>" << std::endl;
-            condition.str("");
-            condition << "WHERE files.dir_type = " << TSK_FS_NAME_TYPE_REG 
-                << " AND files.type_id = " << TskImgDB::IMGDB_FILES_TYPE_FS;
-            out << "<td><b>File System:</b></td>";
-            out << "<td>" << imgdb.getFileCount(condition.str()) << "</td>" << std::endl;
-            out << "</tr>" << std::endl;
-
-            out << "<tr>" << std::endl;
-            condition.str("");
-            condition << "WHERE files.dir_type = " << TSK_FS_NAME_TYPE_REG 
-                << " AND files.type_id = " << TskImgDB::IMGDB_FILES_TYPE_CARVED;
-            out << "<td><b>Carved:</b></td>";
-            out << "<td>" << imgdb.getFileCount(condition.str()) << "</td>" << std::endl;
-            out << "</tr>" << std::endl;
-
-            out << "<tr>" << std::endl;
-            condition.str("");
-            condition << "WHERE files.dir_type = " << TSK_FS_NAME_TYPE_REG 
-                << " AND files.type_id = " << TskImgDB::IMGDB_FILES_TYPE_UNUSED;
-            out << "<td><b>Contiguous Unallocated Sectors:</b></td>";
-            out << "<td>" << imgdb.getFileCount(condition.str()) << "</td>" << std::endl;
-            out << "</tr>" << std::endl;
-
-            out << "<tr>" << std::endl;
-            condition.str("");
-            condition << "WHERE files.dir_type = " << TSK_FS_NAME_TYPE_REG;
-            out << "<td><b>Total Files:</b></td>";
-            out << "<td><b>" << imgdb.getFileCount(condition.str()) << "</b></td>" << std::endl;
-            out << "</tr>" << std::endl;
-
-            out << "<tr>" << std::endl;
-            out << "</table>" << std::endl;
-
-            std::vector<TskBlackboardArtifact> artifacts = blackboard.getMatchingArtifacts("ORDER BY artifact_type_id");
-
-            std::vector<TskBlackboardArtifact>::iterator it;
-
-            int currentArtType = -1;
-            std::vector<int> attrTypeIDs;
-
-            out << "<h2>Blackboard Artifacts</h2>" << std::endl;
-            for(it = artifacts.begin(); it != artifacts.end(); it++){
-                if(currentArtType != it->getArtifactTypeID()){
-                    if(currentArtType != -1)
-                        out << "</tbody>" << std::endl << "</table>" << std::endl;
-                    currentArtType = it->getArtifactTypeID();
-                    out << "<h3>" << it->getDisplayName() << "</h3>" << std::endl;
-                    attrTypeIDs = blackboard.findAttributeTypes(currentArtType);
-                    out << "<table class=\"gridtable\">" << std::endl;
-                    out << "<thead>" << std::endl;
-                    out << "<tr>" << std::endl;
-                    out << "<th>File Name</th>" << std::endl;
-                    for(size_t i = 0; i < attrTypeIDs.size(); i++){
-                        out << "<th>" << blackboard.attrTypeIDToTypeDisplayName(attrTypeIDs[i]) << "</th>" << std::endl;
-                    }
-                    out << "</tr>" << std::endl << "</thead>" << std::endl;
-                    out << "<tbody>" << std::endl;
-                }
-                out << "<tr>" << std::endl;
-                out << "<td>" << imgdb.getFileName(it->getObjectID()) << "</td>" << std::endl;
-                std::vector<TskBlackboardAttribute> attrs = it->getAttributes();
-
-                for (size_t j = 0; j < attrTypeIDs.size(); j++){
-                    TskBlackboardAttribute * attr;
-                    bool found = false;
-                    for(size_t k = 0; k < attrs.size(); k++){
-                        if(attrs[k].getAttributeTypeID() == attrTypeIDs[j]){
-                            attr = &attrs[k];
-                            found = true;
-                            break;
-                        }
-                    }
-                    if(!found)
-                        out << "<td/>" << std::endl;
-                    else{
-                        out << "<td>";
-                        std::vector<unsigned char> bytes;
-                        switch(attr->getValueType()){
-                            case TSK_BYTE:
-                                bytes = attr->getValueBytes();
-                                for(size_t k = 0; k < bytes.size(); k++)
-                                    out << bytes[k];
-                                out << "</td>" << std::endl;
-                                break;
-
-                            case TSK_DOUBLE:
-                                out << attr->getValueDouble() << "</td>" << std::endl;
-                                break;
-
-                            case TSK_INTEGER:
-                                out << attr->getValueInt() << "</td>" << std::endl;
-                                break;
-
-                            case TSK_LONG:
-                                out << attr->getValueLong() << "</td>" << std::endl;
-                                break;
-
-                            case TSK_STRING:
-                                std::string encoded = HTMLEncode(attr->getValueString());
-                                out << encoded << "</td>" << std::endl;
-                                break;
-                        }
-                    }
-                }
-                out << "</tr>" << std::endl;
-            }
-            if(artifacts.size() > 0)
-                out << "</tbody>" << std::endl << "</table>" << std::endl;
-            out << "</body>" << std::endl;
-            out << "</html>" << std::endl;
-
-        }
-        catch (TskException& tskEx)
+     * Module execution function. 
+     *
+     * @returns TskModule::OK on success, TskModule::FAIL on error, or TskModule::STOP.
+     */
+    TskModule::Status TSK_MODULE_EXPORT report() 
+    {
+        std::ostringstream msgPrefix;
+        msgPrefix << MODULE_NAME << "::report : ";
+        try
         {
-            std::stringstream msg;
-            msg << "ReportModule - Caught framework exception: " << tskEx.what();
+            // Create an output folder.
+            Poco::Path outputFolderPath = Poco::Path::forDirectory(GetSystemProperty(TskSystemProperties::MODULE_OUT_DIR));
+            outputFolderPath.pushDirectory(MODULE_NAME);
+            Poco::File(outputFolderPath).createDirectory();
+
+            // Generate the report.
+            outputFolderPath.setFileName("SummaryReport.htm");
+            TskSummaryReport::generateReport(outputFolderPath.toString());
+
+            return TskModule::OK;
+        }
+        catch (TskException &ex)
+        {
+            std::ostringstream msg;
+            msg << msgPrefix.str() << "TskException: " << ex.message();
             LOGERROR(msg.str());
             return TskModule::FAIL;
         }
-        catch (std::exception& ex)
+        catch (Poco::Exception &ex)
         {
-            std::stringstream msg;
-            msg << "ReportModule - Caught exception: " << ex.what();
+            std::ostringstream msg;
+            msg << msgPrefix.str() << "Poco::Exception: " << ex.displayText();
             LOGERROR(msg.str());
             return TskModule::FAIL;
         }
-
-        return TskModule::OK;
+        catch (std::exception &ex)
+        {
+            std::ostringstream msg;
+            msg << msgPrefix.str() << "std::exception: " << ex.what();
+            LOGERROR(msg.str());
+            return TskModule::FAIL;
+        }
+        catch (...)
+        {
+            LOGERROR(msgPrefix.str() + "unrecognized exception");
+            return TskModule::FAIL;
+        }
     }
 
+    /**
+     * Module cleanup function. This is where the module should free any resources 
+     * allocated during initialization or execution.
+     *
+     * @returns TskModule::OK on success and TskModule::FAIL on error.
+     */
     TskModule::Status TSK_MODULE_EXPORT finalize()
     {
-        return TskModule::OK;
+        std::ostringstream msgPrefix;
+        msgPrefix << MODULE_NAME << "::finalize : ";
+        try
+        {
+            // Delete the output folder if it's empty.
+            Poco::Path outputFolderPath = Poco::Path::forDirectory(GetSystemProperty(TskSystemProperties::MODULE_OUT_DIR));
+            outputFolderPath.pushDirectory(MODULE_NAME);
+            Poco::File outputFolder(outputFolderPath);
+            std::vector<Poco::File> filesList;
+            outputFolder.list(filesList);
+            if (filesList.empty())
+            {
+                outputFolder.remove(true);
+            }
+
+            return TskModule::OK;
+        }
+        catch (TskException &ex)
+        {
+            std::ostringstream msg;
+            msg << msgPrefix.str() << "TskException: " << ex.message();
+            LOGERROR(msg.str());
+            return TskModule::FAIL;
+        }
+        catch (Poco::Exception &ex)
+        {
+            std::ostringstream msg;
+            msg << msgPrefix.str() << "Poco::Exception: " << ex.displayText();
+            LOGERROR(msg.str());
+            return TskModule::FAIL;
+        }
+        catch (std::exception &ex)
+        {
+            std::ostringstream msg;
+            msg << msgPrefix.str() << "std::exception: " << ex.what();
+            LOGERROR(msg.str());
+            return TskModule::FAIL;
+        }
+        catch (...)
+        {
+            LOGERROR(msgPrefix.str() + "unrecognized exception");
+            return TskModule::FAIL;
+        }
     }
 }
 
